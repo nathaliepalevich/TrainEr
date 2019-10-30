8379923d3b3f760459277ef3d8eef2326d88449a
Nathalie Palevich <nathaliepalevich@gmail.com>
2019-10-30 10:51:08 +0200

    .

--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e39b4bd181ecd0b1da2ef1d1636c7dba.js"
+  "/precache-manifest.b5fa2fb9363cf6f26d19c3075e2f1ccb.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "final_project"});
