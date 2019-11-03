6b6a7300734a30128d610af6bd8e143c3acab0d4
Nathalie Palevich <nathaliepalevich@gmail.com>
2019-11-03 11:34:10 +0200

    test2

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config(cloudinaryConfig)
 
 if (process.env.NODE_ENV !== "production") {
 app.use(cors({
-    origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082','http://localhost:8086'],
+    origin: ['http://localhost:8080', 'http://localhost:5000','http://localhost:8081', 'http://localhost:8082','http://localhost:8086'],
     credentials: true
 }));
 }
